35fae7557814e35c3752e9d42fe8d4ee908bfa9c
Sonu656 <sk656433@gmail.com>
2025-05-26 15:23:31 +0530

    flask frontend and express backend connect

--- flask-frontend/app.py
+++ flask-frontend/app.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template, request
 import requests
 import os
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 BACKEND_URL = os.getenv("BACKEND_URL", "http://localhost:3000")
 
 @app.route('/')
 def index():
-    
-        return render_template('index.html')
+    try:
+        response = requests.get(f"{BACKEND_URL}/api")
+        data = response.json()
+        return render_template('index.html', backend_message=data['message'])
+    except Exception as e:
+        return f"Error contacting backend: {e}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
